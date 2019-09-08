6ef867a0ad5d189c719adb0dc976d5e0ca9f7f22
yassir ouladbouaziz <ouladbouaziz@gmail.com>
2019-09-08 22:34:19 +0000

    'Update'

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('RDV/',views.res_view,name='res'),
    
 
 ]
--- website/urls.py
+++ website/urls.py
@@ -5,8 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-	path('',include('core.urls')),    
+	path('',views.res_view,name='res'),
+    path('admin/', admin.site.urls),    
 ]
 
 if settings.DEBUG:
