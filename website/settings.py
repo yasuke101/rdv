e83b700b5bee0abfee95383e2657b082723def87
yasuke101 <55018340+yasuke101@users.noreply.github.com>
2019-09-08 20:57:18 +0000

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','j*(#)&loey7@$92t4!a)x1$%=*6xsyz
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rdvtest.herokuapp.com']
 
 
 # Application definition
