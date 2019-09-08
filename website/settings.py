59f4f9aceacc0cbd5db4eaa1f3a12de671ead951
yassir ouladbouaziz <ouladbouaziz@gmail.com>
2019-09-08 22:40:56 +0000

    'Update'

--- website/settings.py
+++ website/settings.py
@@ -124,6 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
