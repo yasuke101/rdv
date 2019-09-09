462ae40c6a39f91dff0c7ee5bd327d85d8be767d
yassir ouladbouaziz <ouladbouaziz@gmail.com>
2019-09-09 18:22:33 +0000

    upload

--- website/settings.py
+++ website/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
+STATIC_URL = '/tmp/build_bb8b7298354b4ff36fe4c65914ec605a/staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
