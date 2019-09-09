a261cfa7c44723cb566eca9f82c02950ddcc9568
yassir ouladbouaziz <ouladbouaziz@gmail.com>
2019-09-09 18:33:36 +0000

    upload

--- website/settings.py
+++ website/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
+STATIC_URL = 'rdv-test.herokuapp.com/staticfiles/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
