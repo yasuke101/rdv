02bd728a87bc177431ed92bc71712bb219b12f20
yassir ouladbouaziz <ouladbouaziz@gmail.com>
2019-09-09 10:20:12 +0000

    'Update'

--- core/models.py
+++ core/models.py
@@ -1,12 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
-
 
 def user_directory_path(instance, filename):
 	return '{0}/{1}'.format(instance.CIN, filename)
-
-class profile(models.Model):
-    user=models.OneToOneField(User, on_delete=models.CASCADE)
 
 class res(models.Model):
 	sexe=[('',''),('Homme','Homme'),('Femme','Femme')]
