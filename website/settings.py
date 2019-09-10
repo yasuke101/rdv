4701a3b2214538b5f93769187a8e886b4e0bf1f3
yassir ouladbouaziz <ouladbouaziz@gmail.com>
2019-09-10 15:24:50 +0000

    upload

--- website/settings.py
+++ website/settings.py
@@ -133,3 +133,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'hamidbattriemamak@gmail.com'
+EMAIL_HOST_PASSWORD = 'yasuke:!:'
