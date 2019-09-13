4d5e1df03ba7350b90114a1b4df115396c5ab27f
yassir ouladbouaziz <ouladbouaziz@gmail.com>
2019-09-13 18:17:52 +0100

    upload

--- core/models.py
+++ core/models.py
@@ -1,12 +1,8 @@
 from django.db import models
 
-#def user_directory_path(instance, filename):
-#	return '{0}/{1}'.format(instance.CIN, filename)
-
 class res(models.Model):
 	sexe=[('',''),('Homme','Homme'),('Femme','Femme')]
 	CIN=models.CharField(max_length=7)
-	#image_de_CIN=models.ImageField(upload_to=user_directory_path)
 	nom=models.CharField(max_length=200)
 	sexe=models.CharField(choices=sexe,max_length=5)
 	prenom=models.CharField(max_length=200)
