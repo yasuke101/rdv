711ffa32d555866fa207d9fb4275bb320a963c68
yassir ouladbouaziz <ouladbouaziz@gmail.com>
2019-09-17 21:22:29 +0100

    upload

--- website/settings.py
+++ website/settings.py
@@ -46,12 +46,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CSRF_COOKIE_SECURE=True
-SECURE_CONTENT_TYPE_NOSNIFF=True
-SECURE_BROWSER_XSS_FILTER=True
-SECURE_SSL_REDIRECT=True
-SESSION_COOKIE_SECURE=True
-X_FRAME_OPTIONS='DENY'
+
 
 ROOT_URLCONF = 'website.urls'
 
